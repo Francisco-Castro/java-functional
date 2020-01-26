18ecfefe440f2cfcef348e131212c14bacba0373
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-01-25 19:44:51 -0500

    Added an example for sorting properties of an object and also limiting the number of the output elements

---
+++ src/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+
+    private String title;
+    private Integer copies;
+
+    public Book(String title, Integer copies) {
+        this.title = title;
+        this.copies = copies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getCopies() {
+        return copies;
+    }
+
+    public void setCopies(Integer copies) {
+        this.copies = copies;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", pages=" + copies +
+                '}';
+    }
+}
---
+++ src/SortPart2Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortPart2Main {
+
+    public static void main(String[] args) {
+
+        List<Book> books = new ArrayList<>();
+
+        books.add(new Book("The Lord of the Rings", 150));
+        books.add(new Book("Don Quijote de la Mancha", 500));
+        books.add(new Book("Story of Two Cities", 200));
+        books.add(new Book("Story of Two Cities", 200));
+        books.add(new Book("El principito", 140));
+
+        Comparator<Book> comparingPages = Comparator.comparing(Book::getCopies);
+
+        List<Book> collect = books.stream()
+                .sorted(comparingPages.reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
+
+
+    }
+}
